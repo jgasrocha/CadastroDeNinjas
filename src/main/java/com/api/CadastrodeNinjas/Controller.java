e557977e749dde258f7a9922068a14b1678587f4
João Gabriel <gabrieldeveloper33@gmail.com>
2025-05-01 12:45:16 -0300

    Criado o Controller

---
+++ src/main/java/com/api/CadastrodeNinjas/Controller.java
@@ -0,0 +1,14 @@
+package com.api.CadastrodeNinjas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVinda() {
+        return "Essa é minha primeira mensagem nesta rota";
+    }
+}
