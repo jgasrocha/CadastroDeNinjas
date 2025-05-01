f41473fb7618f322f2219c28a4f9a4ce6902fe2f
João Gabriel <gabrieldeveloper33@gmail.com>
2025-05-01 12:03:47 -0300

    first commit

---
+++ src/test/java/com/api/CadastrodeNinjas/CadastrodeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.CadastrodeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
